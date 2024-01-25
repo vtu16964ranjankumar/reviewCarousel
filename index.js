af1a16f753223550fae7e68647e07fdd92502681
Ranjan Kumar <vtu16964@veltech.edu.in>
2024-01-26 01:47:02 +0530

    updated

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 const reviews = [
   {
     id: 1,
+    name: 'Ranjan Kumar',
+    job: 'web developer',
+    img: 'Ranjan_Photo.jpg',
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 1,
     name: 'susan smith',
     job: 'web developer',
     img: 'https://www.course-api.com/images/people/person-1.jpeg',
